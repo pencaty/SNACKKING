4ef9a9fa543faaa2bc2d76fa7504f20c19ef3fba
pencaty <rhckdrjs@gmail.com>
2019-12-06 15:57:21 +0900

    Change taste keywords
    Change taste keywords (sour->bland, bitter->nutty)

--- PHP files/update_snack.php
+++ PHP files/update_snack.php
@@ -13,14 +13,14 @@ if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $andr
     $name = $_POST['name'];
     $sweet=(int)($_POST['sweet']);
     $spicy=(int)($_POST['spicy']);
-    $sour=(int)($_POST['sour']);
-    $bitter=(int)($_POST['bitter']);
+    $bland=(int)($_POST['bland']);
+    $nutty=(int)($_POST['nutty']);
     $salty=(int)($_POST['salty']);
     $greasy=(int)($_POST['greasy']);
     $crispy=(int)($_POST['crispy']);
     $moist=(int)($_POST['moist']);
 
-    if(empty($sweet) && empty($spicy) && empty($sour) && empty($bitter) && empty($salty) && empty($greasy) && empty($crispy) && empty($moist)) {
+    if(empty($sweet) && empty($spicy) && empty($bland) && empty($nutty) && empty($salty) && empty($greasy) && empty($crispy) && empty($moist)) {
         $errMSG = "update_snack 점수를 입력하세요.";
     }
 
@@ -33,8 +33,8 @@ if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $andr
             if($row['Name'] == $name){
                 $sweet = $sweet + $row['Sweet'];
                 $spicy = $spicy + $row['Spicy'];
-                $sour = $sour + $row['Sour'];
-                $bitter = $bitter + $row['Bitter'];
+                $bland = $bland + $row['Bland'];
+                $nutty = $nutty + $row['Nutty'];
                 $salty = $salty + $row['Salty'];
                 $greasy = $greasy + $row['Greasy'];
                 $crispy = $crispy + $row['Crispy'];
@@ -42,8 +42,8 @@ if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $andr
 
                 $score_array[0] = $sweet;
                 $score_array[1] = $spicy;
-                $score_array[2] = $sour;
-                $score_array[3] = $bitter;
+                $score_array[2] = $bland;
+                $score_array[3] = $nutty;
                 $score_array[4] = $salty;
                 $score_array[5] = $greasy;
                 $score_array[6] = $crispy;
@@ -81,9 +81,9 @@ if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $andr
                     break;
                     case 1 : $keyword_one = 'Spicy';
                     break;
-                    case 2 : $keyword_one = 'Sour';
+                    case 2 : $keyword_one = 'Bland';
                     break;
-                    case 3 : $keyword_one = 'Bitter';
+                    case 3 : $keyword_one = 'Nutty';
                     break;
                     case 4 : $keyword_one = 'Salty';
                     break;
@@ -102,9 +102,9 @@ if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $andr
                         break;
                     case 1 : $keyword_two = 'Spicy';
                         break;
-                    case 2 : $keyword_two = 'Sour';
+                    case 2 : $keyword_two = 'Bland';
                         break;
-                    case 3 : $keyword_two = 'Bitter';
+                    case 3 : $keyword_two = 'Nutty';
                         break;
                     case 4 : $keyword_two = 'Salty';
                         break;
@@ -123,9 +123,9 @@ if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $andr
                         break;
                     case 1 : $keyword_three = 'Spicy';
                         break;
-                    case 2 : $keyword_three = 'Sour';
+                    case 2 : $keyword_three = 'Bland';
                         break;
-                    case 3 : $keyword_three = 'Bitter';
+                    case 3 : $keyword_three = 'Nutty';
                         break;
                     case 4 : $keyword_three = 'Salty';
                         break;
@@ -141,24 +141,24 @@ if( (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit'])) || $andr
 
                 //여기서 키워드 별 점수를 비교해서 가장 높은 3개를 Keyword_One, Two, Three에 배정한 후 한번에 UPDATE하자.
 
-/*                $update_score = $con->prepare('UPDATE Snack_Score SET Sweet = :sweet, Spicy = :spicy, Sour = :sour, Bitter = :bitter WHERE Name = :name');
+/*                $update_score = $con->prepare('UPDATE Snack_Score SET Sweet = :sweet, Spicy = :spicy, Bland = :bland, Nutty = :nutty WHERE Name = :name');
                 $update_score->bindParam(':name', $name);
                 $update_score->bindParam(':sweet', $sweet);
                 $update_score->bindParam(':spicy', $spicy);
-                $update_score->bindParam(':sour', $sour);
-                $update_score->bindParam(':bitter', $bitter);
+                $update_score->bindParam(':bland', $bland);
+                $update_score->bindParam(':nutty', $nutty);
                 $update_score->execute();
                 break;*/
 
-                $update_score = $con->prepare('UPDATE Snack_Score SET Keyword_One = :keyword1, Keyword_Two = :keyword2, Keyword_Three = :keyword3, Sweet = :sweet, Spicy = :spicy, Sour = :sour, Bitter = :bitter, Salty = :salty, Greasy = :greasy, Crispy = :crispy, Moisturized = :moist WHERE Name = :name');
+                $update_score = $con->prepare('UPDATE Snack_Score SET Keyword_One = :keyword1, Keyword_Two = :keyword2, Keyword_Three = :keyword3, Sweet = :sweet, Spicy = :spicy, Bland = :bland, Nutty = :nutty, Salty = :salty, Greasy = :greasy, Crispy = :crispy, Moisturized = :moist WHERE Name = :name');
                 $update_score->bindParam(':name', $name);
                 $update_score->bindParam(':keyword1', $keyword_one);
                 $update_score->bindParam(':keyword2', $keyword_two);
                 $update_score->bindParam(':keyword3', $keyword_three);
                 $update_score->bindParam(':sweet', $sweet);
                 $update_score->bindParam(':spicy', $spicy);
-                $update_score->bindParam(':sour', $sour);
-                $update_score->bindParam(':bitter', $bitter);
+                $update_score->bindParam(':bland', $bland);
+                $update_score->bindParam(':nutty', $nutty);
                 $update_score->bindParam(':salty', $salty);
                 $update_score->bindParam(':greasy', $greasy);
                 $update_score->bindParam(':crispy', $crispy);
@@ -191,8 +191,8 @@ if( !$android )
         Name: <input type = "text" name = "name" /><br>
         Sweet: <input type = "text" name = "sweet" /><br>
         Spicy: <input type = "text" name = "spicy" /><br>
-        Sour: <input type = "text" name = "sour" /><br>
-        Bitter: <input type = "text" name = "bitter" />
+        Bland: <input type = "text" name = "bland" /><br>
+        Nutty: <input type = "text" name = "nutty" />
         <input type = "submit" name = "submit" />
     </form>
 
